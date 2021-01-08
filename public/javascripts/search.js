75cba7a96246e1fb503a7435081fa51fd6d657a7
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-08 14:18:26 -0800

    added responsive nav bar

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,30 +1,105 @@
-document.addEventListener("DOMContentLoaded", async event => { 
-    // let title = "Avengers Endgame";
-    // let IMDbID = "tt4154796";
+// document.addEventListener("DOMContentLoaded", async event => { 
+//     // let title = "Avengers Endgame";
+//     // let IMDbID = "tt4154796";
 
-    // fetch(`/search?title=${title}`)
-    // .then(res => res.json())
-    // .then(data => console.log(data))
-    // .catch(err => console.error(err));
+//     // fetch(`/search?title=${title}`)
+//     // .then(res => res.json())
+//     // .then(data => console.log(data))
+//     // .catch(err => console.error(err));
 
-    // const searchByTitle = await search(`/search?title=${title}`);
-    // console.log(searchByTitle);
+//     // const searchByTitle = await search(`/search?title=${title}`);
+//     // console.log(searchByTitle);
 
-    // const searchByIMDbID = await search(`/search?id=${IMDbID}`);
-    // console.log(searchByIMDbID);
+//     // const searchByIMDbID = await search(`/search?id=${IMDbID}`);
+//     // console.log(searchByIMDbID);
 
-    // Retrieve and display this weeks DVD releases
-    const response = await fetch("/search/new-releases");
-    const data = await response.json();
-    console.log(`New DVD releases for the week of ${data.weekOf}`);
-    console.table(data.releases);
-});
+//     // Retrieve and display this weeks DVD releases
+//     const response = await fetch("/search/new-releases");
+//     const data = await response.json();
+//     console.log(`New DVD releases for the week of ${data.weekOf}`);
+//     console.table(data.releases);
+// });
 
 
-const search = async url => {
-    const response = await fetch(url);
-    return await response.json();
+// const search = async url => {
+//     const response = await fetch(url);
+//     return await response.json();
+// }
+
+const APIURL =
+"https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+"https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+const resp = await fetch(url);
+const respData = await resp.json();
+
+console.log(respData);
+
+showMovies(respData.results);
 }
+
+function showMovies(movies) {
+// clear main
+main.innerHTML = "";
+
+movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+<img
+    src="${IMGPATH + poster_path}"
+    alt="${title}"
+/>
+<div class="movie-info">
+    <h3>${title}</h3>
+    <span class="${getClassByRate(
+        vote_average
+    )}">${vote_average}</span>
+</div>
+<div class="overview">
+    <h3>Overview:</h3>
+    ${overview}
+</div>
+`;
+
+    main.appendChild(movieEl);
+});
+}
+
+function getClassByRate(vote) {
+if (vote >= 8) {
+    return "green";
+} else if (vote >= 5) {
+    return "orange";
+} else {
+    return "red";
+}
+}
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+
+const searchTerm = search.value;
+
+if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+
+    search.value = "";
+}
+});
 
 
 
