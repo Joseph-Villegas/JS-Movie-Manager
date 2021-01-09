052a81fb6d54bc673813ccba9743c7bc98d81029
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-08 22:46:48 -0800

    showing new releases on home page

---
+++ public/javascripts/index.js
@@ -0,0 +1,47 @@
+/* Filename: public/javascripts/index.js
+ * Abstract: This script contains functions
+ *           specifically related to the page index.hbs
+ */
+
+// Retrieve and display this weeks DVD releases
+const getNewReleases = async () => {
+    const response = await fetch("/search/new-releases");
+    const data = await response.json();
+    console.log(`New DVD releases for the week of ${data.weekOf}`);
+    console.table(data.releases);
+    showMovies(data.releases);
+};
+
+document.addEventListener("DOMContentLoaded", getNewReleases);
+
+function showMovies(movies) {
+    // clear main
+    main.innerHTML = "";
+
+    const headerElement = document.createElement("h2");
+    headerElement.classList.add("header");
+    headerElement.innerHTML = `New This Week`;
+    main.appendChild(headerElement);
+
+    const searchResults = document.createElement("div");
+    searchResults.classList.add("search-results");
+
+    movies.forEach((movie) => {
+        const { poster, title } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img src="${poster}" alt="${title}"/>
+            <div class="movie-info">
+                <h3><a href="#" aria-label="See info for ${title}">${title}</a></h3>
+            </div>
+        `;
+
+        searchResults.appendChild(movieEl);
+    });
+
+    main.appendChild(searchResults);
+}
+
