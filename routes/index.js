9c07d85ca6c068fa85bc36f5205ac32bfad72622
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-08 21:52:11 -0800

    added login route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Movie Manager' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Movie Manager' });
+});
+
 module.exports = router;
