38983c8a41333fad90bea245e5e742dcbce9e6bb
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-09 22:12:55 -0800

    added routes to new pages

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Movie Manager' });
+router.get('/', (req, res) => {
+  let data = { title: 'Movie Manager' };
+
+  if (req.session.user) {
+    data.username = req.session.user.username;
+  }
+
+  res.render('index', data);
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/dashboard');
+  }
+
   res.render('login', { title: 'Movie Manager' });
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', (req, res) => {
+  if (req.session.user) {
+    res.redirect('/dashboard');
+  }
+
   res.render('register', { title: 'Movie Manager' });
+});
+
+router.get('/dashboard', (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/login');
+  }
+
+  res.render('dashboard', { title: 'Movie Manager', username: req.session.user.username });
 });
 
 module.exports = router;
