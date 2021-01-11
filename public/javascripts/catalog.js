77ef88319d755d415ec658b422ba8358058663b2
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-10 20:54:45 -0800

    retrieve catalog

--- public/javascripts/catalog.js
+++ public/javascripts/catalog.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    console.log("In catalog page");
+    // Retrieve the user's catalog
+    const response = await fetch(`/lists/catalog`);
+    const data = await response.json();
+    console.table(data.catalog);
+
+    // If no user is logged in no catalog is to be fetched, go home
+    if (!data.success) {
+        window.location.href = "/";
+        return;
+    }
+
+    const numFilmsInCatalog = data.length;
+    console.log(`Number of films in catalog: ${numFilmsInCatalog}`);
+
+    const headerElement = document.createElement("h2");
+    headerElement.classList.add("header");
+    headerElement.innerHTML = `${numFilmsInCatalog} Films in Catalog`;
+    main.appendChild(headerElement);
 });
