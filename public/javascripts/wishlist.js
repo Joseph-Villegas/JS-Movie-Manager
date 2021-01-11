6d6942adfcbe1b58eb51a850dbbec218ba51d557
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-10 21:16:29 -0800

    showing lists on catalog and wish list pages

--- public/javascripts/wishlist.js
+++ public/javascripts/wishlist.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    // Retrieve the user's catalog
-    const response = await fetch(`/lists/catalog`);
+    // Retrieve the user's wish list
+    const response = await fetch(`/lists/wish-list`);
     const data = await response.json();
 
-    // If no user is logged in no catalog is to be fetched, go home
+    // If no user is logged in no wish list is to be fetched, go home
     if (!data.success) {
         window.location.href = "/";
         return;
     }
 
-    console.table(data.catalog);
+    console.table(data.wish_list);
 
-    const numFilmsInCatalog = data.length;
-    console.log(`Number of films in catalog: ${numFilmsInCatalog}`);
+    const numFilmsInWishList = data.length;
+    console.log(`Number of films in wish list: ${numFilmsInWishList}`);
 
     // Display how many search results were found
     const headerElement = document.createElement("h2");
     headerElement.classList.add("header");
-    headerElement.innerHTML = `${numFilmsInCatalog} Film(s) in Catalog`;
+    headerElement.innerHTML = `${numFilmsInWishList} Film(s) in Wish List`;
     main.appendChild(headerElement);
 
-    // Display all movies in the user's catalog
-    displayCatalog(data.catalog);
+    // Display all movies in the user's wish list
+    displayWishList(data.wish_list);
 });
 
-const displayCatalog = movies => {
+const displayWishList = movies => {
     const searchResults = document.createElement("div");
     searchResults.classList.add("search-results");
 
     movies.forEach((movie) => {
-        const { title, year, imdbId, poster, copies } = movie;
+        const { title, year, imdbId, poster } = movie;
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
 
@@ -37,7 +37,6 @@ const displayCatalog = movies => {
             <img src="${poster}" alt="${title}"/>
             <div class="movie-info">
                 <h3><a href="/movie/${imdbId}" aria-label="See info for ${title}">${title} (${year})</a></h3>
-                <span>x${copies}</span>
             </div>
         `;
 
