8356af8565c26f4861e7bd9b3738362f01444942
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-12 20:23:21 -0800

    control what list options are available

--- public/javascripts/movie.js
+++ public/javascripts/movie.js
@@ -1,98 +1,50 @@
-const imdbId = document.getElementById("imdbId").textContent;
 document.addEventListener("DOMContentLoaded", async () => {
-    console.log(`In movie page, showing infor for movie with id ${imdbId}`);
-
-    const rawURL = `/search?id=${imdbId}`;
-    const url = encodeURI(rawURL);
+    const imdbId = document.getElementById("imdbId").textContent;
+    console.log(`In movie page, showing info for movie with id ${imdbId}`);
 
     // Search for film
-    const response = await fetch(url);
+    const response = await fetch(`/search?id=${imdbId}`);
     const data = await response.json();
     console.log(data);
 
-    if (!data.success) {
-        console.log("No movie found with matching ID");
-        return;
-    }
+    if (!data.success || (data.films.Poster == "N/A" || data.films.Type == "game")) window.location.href = "/";
 
-    const { Actors, Director, Genre, Plot, Poster, Rated, Released, Title, Type, Writer, imdbRating } = data.films;
+    const { Poster, Title, Year, imdbID } = data.films;
 
-    if (Poster == "N/A" || Type == "game") {
-        console.log("Invalid or missing attr.");
-        return;
-    }
-    
-    const container = document.createElement("div");
-    container.classList.add("container-fluid");
+    showMovie(data.films);
 
-    const movie = document.createElement("div");
-    movie.setAttribute("id", "movie");
-
-    container.appendChild(movie);
-    
-    main.appendChild(container);
-
-    let output =`
-    <div class="row">
-        <div class= "col-md-4">
-            <img src=${Poster} alt=${Title}>
-        </div>
-        <div class="col-md-8">
-            <h2>${Title}</h2>
-            <ul class="list-group">
-                <li class="list-group-item"><strong>Genre:</strong> ${Genre}</li>
-                <li class="list-group-item"><strong>Released:</strong> ${Released}</li>
-                <li class="list-group-item"><strong>Rated:</strong> ${Rated}</li>
-                <li class="list-group-item"><strong>IMDB Rating:</strong> ${imdbRating}/10</li>
-                <li class="list-group-item"><strong>Director:</strong> ${Director}</li>
-                <li class="list-group-item"><strong>Writer(s):</strong> ${Writer}</li>
-                <li class="list-group-item"><strong>Actors:</strong> ${Actors}</li>
-            </ul>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Plot</h3>
-            ${Plot}
-            <hr style="color:white;">
-            <a href="http://imdb.com/title/${imdbId}" target="_blank" class="btn btn-primary">View on IMDB</a>
-            <button type="button" id="a-c" class="btn btn-success">Add to Catalog</button>
-            <button type="button" id="a-w" class="btn btn-success">Add to Wish List</button>
-            <button type="button" id="r-c" class="btn btn-danger">Remove from Catalog</button>
-            <button type="button" id="r-w" class="btn btn-danger">Remove from WishList</button>
-        </div>
-    </div>
-    `;
-
-    movie.innerHTML = output;
+    defineListPermissions(imdbId);
 
     const addToCatalogButton = document.getElementById("a-c");
     addToCatalogButton.addEventListener("click", () => {
-        addToCatalog(data.films);
+        addToList("/lists/add-to-catalog", { title: Title, year: Year, imdbId: imdbID, poster: Poster, copies: 1 });
     });
 
     const removeFromCatalogButton = document.getElementById("r-c");
     removeFromCatalogButton.addEventListener("click", () => {
-        removeFromCatalog(data.films);
+        removeFromList("/lists/remove-from-catalog", { imdbId: imdbID });
     });
 
-    defineListPermissions();
+    const addToWishListButton = document.getElementById("a-w");
+    addToWishListButton.addEventListener("click", () => {
+        addToList("/lists/add-to-wish-list", { title: Title, year: Year, imdbId: imdbID, poster: Poster });
+    });
+
+    const removeFromWishListButton = document.getElementById("r-w");
+    removeFromWishListButton.addEventListener("click", () => {
+        removeFromList("/lists/remove-from-wish-list", { imdbId: imdbID });
+    });
 });
 
-// Define list permissions
-const defineListPermissions = async () => {
-    const addToCatalog = document.getElementById("a-c");
-    const addToWishList = document.getElementById("a-w");
-
-    const removeFromCatalog = document.getElementById("r-c");
-    const removeFromWishList = document.getElementById("r-w");
-
+/**
+ * Defines possible list actions by checking if a movie is in the user's catalog or wish list
+ * @param imdbId String
+ * @returns void
+ */
+const defineListPermissions = async (imdbId) => {
     if (!(await checkLoginStatus())) {
         console.log("No user logged in");
-        addToCatalog.style.display = 'none';
-        addToWishList.style.display = 'none';
-        removeFromCatalog.style.display = 'none';
-        removeFromWishList.style.display = 'none';
+        setDisplayOptions(0, 0, 0, 0);
         return;
     }
 
@@ -119,96 +71,149 @@ const defineListPermissions = async () => {
 
     // If the movie is not in either, display the add to catalog and wish list buttons
     if (!inCatalog && !inWishList) {
-        addToCatalog.style.display = 'inline-block';
-        addToWishList.style.display = 'inline-block';
-        removeFromCatalog.style.display = 'none';
-        removeFromWishList.style.display = 'none';
+        setDisplayOptions(1, 1, 0, 0);
         return;
     }
 
     // If the movie is in the catalog, only show the remove from catalog button
     if (inCatalog && !inWishList) {
-        addToCatalog.style.display = 'none';
-        addToWishList.style.display = 'none';
-        removeFromCatalog.style.display = 'inline-block';
-        removeFromWishList.style.display = 'none';
+        setDisplayOptions(0, 0, 1, 0);
         return;
     }
 
     // If the movie is in the wish list, display the add to catalog button and remove from wish list buttons
     if (!inCatalog && inWishList) {
-        addToCatalog.style.display = 'inline-block';
-        addToWishList.style.display = 'none';
-        removeFromCatalog.style.display = 'none';
-        removeFromWishList.style.display = 'inline-block';
+        setDisplayOptions(1, 0, 0, 1);
         return;
     }
 };
 
+/**
+ * Checks if a user is logged in or not
+ * @returns true or false
+ */
 const checkLoginStatus = async() => {
     const loginResponse = await fetch(`/users`);
     const loginStatus = await loginResponse.json();
     return loginStatus.logged_in;
 };
 
-const addToCatalog = async (movie) => {
-    console.log(`Adding ${movie.Title} to catalog`);
+/**
+ * Adds a movie to a logged in user's catalog or wish list, then redefines possible list button options
+ * @param url String
+ * @param movie Object
+ * @returns void
+ */
+const addToList = async (url, movie) => {
+    console.log(`Sending info to route: ${url}`);
 
     setListOptions(true);
-    
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({ title: movie.Title, year: movie.Year, imdbId: movie.imdbID, poster: movie.Poster, copies: 1 })
-    };
 
-    const response = await fetch(`/lists/add-to-catalog`, options);
+    const options = { method: "POST", headers: { "Content-type": "application/json" }, body: JSON.stringify(movie) };
+
+    const response = await fetch(url, options);
     const data = await response.json();
 
     console.log(data);
 
     if (!data.success) {
-        console.log("Could not add to catalog");
+        console.log(`Route - ${url} - could not process data`);
+        setListOptions(false);
         return;
     }
 
-
-    await defineListPermissions();
+    await defineListPermissions(movie.imdbId);
     setListOptions(false);
 };
 
-const removeFromCatalog = async (movie) => {
-    console.log(`Removing ${movie.Title} from catalog`);
+/**
+ * Removes a movie from a logged in user's catalog or wish list, then redefines possible list button options
+ * @param url String
+ * @param movie Object
+ * @returns void
+ */
+const removeFromList = async (url, movie) => {
+    console.log(`Sending info for ${movie.imdbId} to endpoint ${url} for removal`);
 
     setListOptions(true);
     
-    const options = {
-        method: "DELETE",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({ imdbId: movie.imdbID })
-    };
+    const options = { method: "DELETE", headers: { "Content-type": "application/json" }, body: JSON.stringify(movie) };
 
-    const response = await fetch(`/lists/remove-from-catalog`, options);
+    const response = await fetch(url, options);
     const data = await response.json();
 
     console.log(data);
 
     if (!data.success) {
-        console.log("Could not remove from catalog");
+        console.log(`Could not remove ${movie.imdbId} using endpoint ${url}`);
+        setListOptions(false);
         return;
     }
 
-    await defineListPermissions();
+    await defineListPermissions(movie.imdbId);
     setListOptions(false);
 };
 
+/**
+ * Decides whether or not to disable list buttons
+ * @param selection true or false
+ * @returns void
+ */
 const setListOptions = (selection) => { 
     document.getElementById("a-c").disabled = selection;
     document.getElementById("a-w").disabled = selection;
     document.getElementById("r-c").disabled = selection;
     document.getElementById("r-w").disabled = selection;
+};
+
+/**
+ * Appends html containing movie info to movie.hbs
+ * @param movie Object
+ * @returns void
+ */
+const showMovie = (movie) => {
+    const { Actors, Director, Genre, Plot, Poster, Rated, Released, Title, Writer, imdbRating } = movie;
+
+    const poster = document.getElementById("movie-poster");
+    poster.innerHTML = `<img src=${Poster} alt=${Title}>`;
+
+    const stats = document.getElementById("movie-stats");
+    stats.innerHTML = `
+        <h2>${Title}</h2>
+        <ul class="list-group">
+            <li class="list-group-item"><strong>Genre:</strong> ${Genre}</li>
+            <li class="list-group-item"><strong>Released:</strong> ${Released}</li>
+            <li class="list-group-item"><strong>Rated:</strong> ${Rated}</li>
+            <li class="list-group-item"><strong>IMDB Rating:</strong> ${imdbRating}/10</li>
+            <li class="list-group-item"><strong>Director:</strong> ${Director}</li>
+            <li class="list-group-item"><strong>Writer(s):</strong> ${Writer}</li>
+            <li class="list-group-item"><strong>Actors:</strong> ${Actors}</li>
+        </ul>
+    `;
+     
+    const plot = document.getElementById("movie-plot");
+    plot.innerHTML = `
+        <h3>Plot</h3>
+        ${Plot}
+        <hr style="color:white;">
+        <a href="http://imdb.com/title/${imdbId}" target="_blank" class="btn btn-primary">View on IMDB</a>
+    `;
+
+    return
+};
+
+/**
+ * Sets status of list buttons to inline-block or none
+ * @param ac 0 or 1, decides whether or not to show a button
+ * @param aw 0 or 1, decides whether or not to show a button
+ * @param rc 0 or 1, decides whether or not to show a button
+ * @param rw 0 or 1, decides whether or not to show a button
+ * @returns void
+ */
+const setDisplayOptions = (ac, aw, rc, rw) => {
+    // 0 => false hide , 1 => true show
+    document.getElementById("a-c").style.display = ac == 1 ? 'inline-block' : 'none';
+    document.getElementById("a-w").style.display = aw == 1 ? 'inline-block' : 'none';
+    document.getElementById("r-c").style.display = rc == 1 ? 'inline-block' : 'none';
+    document.getElementById("r-w").style.display = rw == 1 ? 'inline-block' : 'none';
 };
