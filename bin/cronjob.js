c54c7c72c7340c82affd0c218d7beaed77b44de9
Joseph-Villegas <jovillegas@csumb.edu>
2021-01-29 23:06:34 -0800

    added heroku schedular compatible methods to get new dvd release info

---
+++ bin/cronjob.js
@@ -0,0 +1,62 @@
+const cron = require('node-cron');
+
+const fetch = require("node-fetch");
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+ 
+const adapter = new FileSync('db.json');
+const db = low(adapter);
+
+// Set db defaults
+db.defaults({ releases: [], scrape_date: "", week_of: "" }).write();
+
+/**
+ * Resets keys in "db.json" file acting as a database
+ */
+const reset = () => {
+    db.set('releases', [])
+      .write();
+    db.set('scrape_date', "")
+      .write();
+    db.set('week_of', "")
+      .write();
+}
+
+/**
+ * Sets key values in "db.json" file acting as a database
+ * @param releases object array of new DVD releases
+ * @param scrape_date date object detailing at what point the data was scraped
+ * @param week_of string detailing the date for the new releases
+ */
+const save = (releases, scrape_date, week_of) => {
+    db.set('releases', releases)
+      .write();
+    db.set('scrape_date', scrape_date)
+      .write();
+    db.set('week_of', week_of)
+      .write();
+}
+
+/**
+ * Retrieves new DVD release info from a third party API
+ */
+const getReleases = async () => {
+    const response = await fetch("https://dvd-release-dates.herokuapp.com/this-week");
+    const data = await response.json();
+    return data;
+};
+
+/**
+ * Retrieves and stores new DVD release info
+ */
+const scrapeAndSave = async () => {
+    reset();
+    const { releases, weekOf: week_of} = await getReleases();
+    const scrape_date = new Date();
+    save(releases, scrape_date, week_of);
+
+    console.log("Scraped and Saved");
+};
+
+scrapeAndSave();
